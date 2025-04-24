2b9517a65a8e17be549f39d2b4e3dea0690087c1
Taisiia-Hulko <162361499+Taisiia-Hulko@users.noreply.github.com>
2025-04-24 08:59:18 +0300

    Add files via upload

---
+++ program3/program3/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+
+class MatrixUlong
+{
+    protected ulong[,] ULArray;
+    protected uint n, m;
+    protected int codeError;
+    protected static int num_m = 0;
+
+    public MatrixUlong()
+    {
+        n = m = 1;
+        ULArray = new ulong[1, 1];
+        ULArray[0, 0] = 0;
+        num_m++;
+    }
+
+    public MatrixUlong(uint rows, uint cols)
+    {
+        n = rows;
+        m = cols;
+        ULArray = new ulong[n, m];
+        codeError = 0;
+        num_m++;
+    }
+
+    public MatrixUlong(uint rows, uint cols, ulong initValue)
+    {
+        n = rows;
+        m = cols;
+        ULArray = new ulong[n, m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                ULArray[i, j] = initValue;
+        codeError = 0;
+        num_m++;
+    }
+
+    ~MatrixUlong()
+    {
+        Console.WriteLine("Matrix destroyed");
+    }
+
+    public void Input()
+    {
+        Console.WriteLine("Enter matrix elements:");
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                ULArray[i, j] = ulong.Parse(Console.ReadLine());
+    }
+
+    public void Output()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+                Console.Write(ULArray[i, j] + " ");
+            Console.WriteLine();
+        }
+    }
+
+    public void Assign(ulong value)
+    {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                ULArray[i, j] = value;
+    }
+
+    public static int CountMatrices() => num_m;
+
+    public uint Rows => n;
+    public uint Columns => m;
+
+    public int CodeError
+    {
+        get => codeError;
+        set => codeError = value;
+    }
+
+    public ulong this[int i, int j]
+    {
+        get
+        {
+            if (i >= 0 && i < n && j >= 0 && j < m)
+                return ULArray[i, j];
+            codeError = -1;
+            return 0;
+        }
+        set
+        {
+            if (i >= 0 && i < n && j >= 0 && j < m)
+                ULArray[i, j] = value;
+            else
+                codeError = -1;
+        }
+    }
+
+    public ulong this[int k]
+    {
+        get
+        {
+            int i = k / (int)m;
+            int j = k % (int)m;
+            if (i >= 0 && i < n && j >= 0 && j < m)
+                return ULArray[i, j];
+            codeError = -1;
+            return 0;
+        }
+        set
+        {
+            int i = k / (int)m;
+            int j = k % (int)m;
+            if (i >= 0 && i < n && j >= 0 && j < m)
+                ULArray[i, j] = value;
+            else
+                codeError = -1;
+        }
+    }
+
+    public static MatrixUlong operator ++(MatrixUlong mat)
+    {
+        for (int i = 0; i < mat.n; i++)
+            for (int j = 0; j < mat.m; j++)
+                mat.ULArray[i, j]++;
+        return mat;
+    }
+
+    public static MatrixUlong operator --(MatrixUlong mat)
+    {
+        for (int i = 0; i < mat.n; i++)
+            for (int j = 0; j < mat.m; j++)
+                mat.ULArray[i, j]--;
+        return mat;
+    }
+
+    public static bool operator true(MatrixUlong mat)
+    {
+        if (mat.n == 0 || mat.m == 0) return false;
+        for (int i = 0; i < mat.n; i++)
+            for (int j = 0; j < mat.m; j++)
+                if (mat.ULArray[i, j] != 0)
+                    return true;
+        return false;
+    }
+
+    public static bool operator false(MatrixUlong mat) => !(mat);
+
+    public static bool operator !(MatrixUlong mat)
+    {
+        return mat.n == 0 || mat.m == 0;
+    }
+
+    public static MatrixUlong operator ~(MatrixUlong mat)
+    {
+        MatrixUlong result = new MatrixUlong(mat.n, mat.m);
+        for (int i = 0; i < mat.n; i++)
+            for (int j = 0; j < mat.m; j++)
+                result.ULArray[i, j] = ~mat.ULArray[i, j];
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MatrixUlong m1 = new MatrixUlong(2, 2, 5);
+        m1.Output();
+        Console.WriteLine("After ++:");
+        m1++;
+        m1.Output();
+
+        Console.WriteLine("Matrix Count: " + MatrixUlong.CountMatrices());
+    }
+}
